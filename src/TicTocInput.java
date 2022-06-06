ae8ef0597cb878b3053705cd98dbfe5ce0c5d778
sravan <sravan.k.vodnala@gmail.com>
2022-06-06 21:46:40 +0530

    Added: allow player to input x or 0

--- src/TicTocInput.java
+++ src/TicTocInput.java
@@ -1,3 +1,30 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
 public class TicTocInput {
+
+    public static char[] gameBoard = new char[10];
+    public static char userTurn;
+    public static char computerTurn;
+    public static char user = '1';
+    public  static Scanner scanner = new Scanner(System.in);
+
+    public static void showBard() {
+
+        System.out.println(gameBoard[0] + " | " + gameBoard[1] + " | " + gameBoard[2]);
+        System.out.println(gameBoard[3] + " | " + gameBoard[4] + " | " + gameBoard[5]);
+        System.out.println(gameBoard[6] + " | " + gameBoard[7] + " | " + gameBoard[8]);
+    }
+
+    public static char user() {
+        System.out.println("Enter your choice x or 0 only");
+        return scanner.next().toUpperCase().charAt(0);
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Tic Toc Toe game");
+        showBard();
+        user();
+    }
 
 }
